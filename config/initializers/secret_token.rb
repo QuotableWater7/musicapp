5214282c0aad38a30eb6bddd2cb75647e77653d4
JT Bowler <jbowler2400@gmail.com>
2014-06-20 16:25:54 -0700

    Pushing files for first time

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musicapp::Application.config.secret_token = 'af4654f2cfca4fd8a2ce110ba797d534f61d5522ec75ad9f88d473941f3f0ef109e3cf7b4d02ac9e2a26a0a834256e7f4b0814ba1aaabc783ae5ea087ae02f5c'
