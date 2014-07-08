a91400bad3cf2a81a77f2ca7439d0cdf0f9841d5
JT Bowler <jbowler2400@gmail.com>
2014-07-08 09:59:38 -0700

    Added some views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    flash[:success] = 'You have successfully tried logging in'
+    redirect_to root_path
   end
 
   def destroy
-
+    flash[:success] = 'You have successfully attempted to log out.'
+    redirect_to root_path
   end
 
 end
