032cbb8eebaf4303414becb5f4368f41e4e6d724
JT Bowler <jbowler2400@gmail.com>
2014-07-09 09:57:24 -0700

    Adding validations on users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
 
   has_secure_password
+
+  validates :email, presence: true
+  validates :password, confirmation: true, length: { minimum: 8, maximum: 20 }
+  validates :password_confirmation, presence: true
 end
