27a66824b81ac3567ce04788647305993d5905df
JT Bowler <jbowler2400@gmail.com>
2014-07-18 20:07:22 -0700

    In the middle of shit...

---
+++ app/assets/javascripts/backbone/views/tab/visualization_view.js
@@ -0,0 +1,19 @@
+(function () {
+  'use strict';
+
+  var VisualizationView = Backbone.View.extend({
+    template: _.template($('#visualization-view').html()),
+
+    events: {
+
+    },
+
+    initialize: function () {
+
+    },
+
+    render: function () {
+
+    }
+  });
+})();
