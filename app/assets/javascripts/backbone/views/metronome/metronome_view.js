bc43f0c3f50e9a7eab4a6c51b0724c1845a80756
JT Bowler <jbowler2400@gmail.com>
2014-07-30 09:55:14 -0700

    Play and pause button switches for metronome sound

--- app/assets/javascripts/backbone/views/metronome/metronome_view.js
+++ app/assets/javascripts/backbone/views/metronome/metronome_view.js
@@ -1,11 +1,11 @@
 (function () {
   'use strict';
 
+  var toggle_state = 'off';
+  var interval_id;
   var sound = new Howl({
     urls: ['/assets/snare-01.wav']
   });
-
-  var toggle_state = 'off';
 
   var MetronomeView = Backbone.View.extend({
     template: _.template($('#metronome-view').html()),
@@ -36,12 +36,25 @@
 
     turnOn: function () {
       toggle_state = 'on';
+      interval_id = setInterval(function () {
+        sound.play();
+      }, 2000);
+
+      this.$el.find('.metronome-toggle')
+        .removeClass('btn-success')
+        .addClass('btn-danger')
+        .text('Pause');
     },
 
     turnOff: function () {
       toggle_state = 'off';
-    }
+      clearInterval(interval_id);
 
+      this.$el.find('.metronome-toggle')
+        .removeClass('btn-danger')
+        .addClass('btn-success')
+        .text('Play');
+    }
   });
 
   App.Views.MetronomeView = MetronomeView;
