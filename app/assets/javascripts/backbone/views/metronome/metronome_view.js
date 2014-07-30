872d9b51c2867166f149cad737b0966b0b56e94b
JT Bowler <jbowler2400@gmail.com>
2014-07-30 16:53:27 -0700

    Saving work; need to get timing mechanism down

--- app/assets/javascripts/backbone/views/metronome/metronome_view.js
+++ app/assets/javascripts/backbone/views/metronome/metronome_view.js
@@ -3,25 +3,30 @@
 
   var toggle_state = 'off';
   var interval_id;
+  var timeout_id;
   var sound = new Howl({
     urls: ['/assets/snare-01.wav']
   });
+  var bpm;
+  var time_to_wait = 500;   // in milliseconds
+  var last_keystroke = new Date().getTime() - time_to_wait;
 
   var MetronomeView = Backbone.View.extend({
     template: _.template($('#metronome-view').html()),
 
     events: {
-      'click .metronome-toggle': 'toggle'
+      'click .metronome-toggle': 'toggle',
+      'keyup .metronome-bpm': 'updateBpm'
     },
 
     initialize: function () {
       _.bindAll(this, 'render');
 
       this.$el.html(this.template());
+      bpm = this.$el.find('.metronome-bpm').val();
     },
 
     render: function () {
-
       return this;
     },
 
@@ -36,9 +41,11 @@
 
     turnOn: function () {
       toggle_state = 'on';
+
+      sound.play();   // play first beat immediately
       interval_id = setInterval(function () {
         sound.play();
-      }, 2000);
+      }, 60*1000/bpm);
 
       this.$el.find('.metronome-toggle')
         .removeClass('btn-success')
@@ -54,6 +61,20 @@
         .removeClass('btn-danger')
         .addClass('btn-success')
         .text('Play');
+    },
+
+    updateBpm: function () {
+      var current_time = new Date().getTime();
+      var new_bpm = parseInt(this.$el.find('.metronome-bpm').val());
+      var enough_time_passed = current_time - last_keystroke > time_to_wait;
+
+      last_keystroke = current_time;
+
+      if (enough_time_passed && new_bpm && new_bpm <= 240) {
+        bpm = new_bpm;
+        this.turnOff();
+        this.turnOn();
+      }
     }
   });
 
