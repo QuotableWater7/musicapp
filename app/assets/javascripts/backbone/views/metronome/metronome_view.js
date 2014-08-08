e25145e2f19373f7126b409e0444865fde82b165
JT Bowler <jbowler2400@gmail.com>
2014-08-08 10:02:18 -0700

    Metronome can now auto speed up by 4bpm

--- app/assets/javascripts/backbone/views/metronome/metronome_view.js
+++ app/assets/javascripts/backbone/views/metronome/metronome_view.js
@@ -1,9 +1,11 @@
 (function () {
   'use strict';
 
+  var autoSpeedTimeout = 5;   // seconds between speeding up metronome
   var toggle_state = 'off';
   var interval_id;
   var timeout_id;
+  var speedup_timeout_id;
   var sound = new Howl({
     urls: ['/assets/snare-01.wav']
   });
@@ -16,12 +18,12 @@
     events: {
       'click .metronome-toggle': 'toggle',
       'keyup .metronome-bpm': 'startCountdown',
-      'click .btn-preset-bpm': 'updateBpm',
+      'click .btn-preset-bpm': 'presetBpm',
       'click .btn-speedup': 'autoSpeed'
     },
 
     initialize: function () {
-      _.bindAll(this, 'render');
+      _.bindAll(this, 'render', 'autoSpeed');
 
       this.$el.html(this.template());
       bpm = this.$el.find('.metronome-bpm').val();
@@ -72,14 +74,13 @@
       timeout_id = setTimeout(
         function () {
           bpm = self.$el.find('.metronome-bpm').val();
-          self.updateBpm(evt);
+          self.refreshBpm(evt);
         },
         time_to_wait
       );
     },
 
-    // this handles updating the bpm from either button click or the countdown
-    updateBpm: function (evt) {
+    presetBpm: function (evt) {
       var $target = $(evt.target);
       var current_time = new Date().getTime();
 
@@ -89,12 +90,25 @@
         this.$el.find('.metronome-bpm').val(bpm);
       }
 
+      this.refreshBpm();
+    },
+
+    // this handles updating the bpm from either button click or the countdown
+    refreshBpm: function (evt) {
+      this.$el.find('.metronome-bpm').val(bpm);
+
       this.turnOff();
       this.turnOn();
     },
 
     autoSpeed: function (evt) {
-      alert('Not yet implemented');
+      var bpm_int = parseInt(bpm);
+
+      if (bpm_int < 100) {
+        bpm = bpm_int + 4;
+        this.refreshBpm();
+        setTimeout(this.autoSpeed, autoSpeedTimeout * 1000);
+      }
     }
   });
 
