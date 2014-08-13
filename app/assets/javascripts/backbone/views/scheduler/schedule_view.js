49b7e9376825057b93b7213e5b27edce23b48a7a
JT Bowler <jbowler2400@gmail.com>
2014-08-12 18:12:20 -0700

    Timer stops when it runs out of schedule items

--- app/assets/javascripts/backbone/views/scheduler/schedule_view.js
+++ app/assets/javascripts/backbone/views/scheduler/schedule_view.js
@@ -6,14 +6,15 @@
   var total_importance = 0;
   var total_time = 30 * 60;
 
-  var timeScale = function () { return total_time / total_importance; };
+  function timeScale() { return total_time / total_importance; }
+  function hasNextActivity() { return schedule_index < schedule_items.length; }
   var timer;
 
   var ScheduleView = Backbone.View.extend({
     template: _.template($('#schedule-view').html()),
 
     events: {
-      'nextActivity': 'loadTimer'
+      'nextActivity': 'nextActivity'
     },
 
     initialize: function () {
@@ -40,13 +41,13 @@
         total_importance += parseInt(item.importance);
       });
 
-      this.loadTimer();
+      this.nextActivity();
 
       return this;
     },
 
-    loadTimer: function () {
-      if (schedule_items) {
+    nextActivity: function () {
+      if (schedule_items && hasNextActivity()) {
         var activity = schedule_items[schedule_index];
         var seconds = parseInt(activity.importance) * timeScale();
 
