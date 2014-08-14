8f794e12382bc86e6d54dd73a1ed66815e681108
JT Bowler <jbowler2400@gmail.com>
2014-08-14 08:54:57 -0700

    Breaks between the schedule items

--- app/assets/javascripts/backbone/views/scheduler/timer_view.js
+++ app/assets/javascripts/backbone/views/scheduler/timer_view.js
@@ -1,17 +1,15 @@
 (function () {
   'use strict';
 
-  var countdown = new App.Countdown().init({
-    seconds: 30,
-    minutes: 10,
-    hours: 0
-  });
+  var countdown;
+  var break_duration = { minutes: 5 };
+  var break_time = false;
 
   var TimerView = Backbone.View.extend({
     template: _.template($('#schedule-timer').html()),
 
     events: {
-      '.start-countdown': 'startCountdown'
+      'finished': 'breakTime'
     },
 
     initialize: function () {
@@ -21,16 +19,30 @@
     render: function () {
       this.$el.html(this.template());
 
+      countdown = new App.Countdown().init({
+        seconds: 30,
+        minutes: 10,
+        hours: 0,
+        $el: this.$el.find('.timer')
+      });
+
       return this;
     },
 
     startCountdown: function (name, opts) {
       this.$el.find('.activity-name').text(name);
-      countdown.start(this.$el.find('.timer'), opts);
+      countdown.start(opts);
     },
 
-    previous: function () {
+    breakTime: function () {
+      if (break_time) {
+        this.$el.trigger('nextActivity');
+      } else {
+        this.$el.find('.activity-name').text('Break');
+        countdown.start(break_duration);
+      }
 
+      break_time = !break_time;
     }
   });
 
--- app/assets/javascripts/countdown.js
+++ app/assets/javascripts/countdown.js
@@ -6,37 +6,34 @@ App.Countdown = function () {
   // public
   self.init = init;
   self.start = start;
-  self.pause = pause;
 
   // private
   var seconds;
   var interval_id;
+  var $el;
 
   function init(opts) {
-    seconds = parseInt(opts.seconds);
+    seconds = 0;
 
+    if (opts.seconds) { seconds = parseInt(opts.seconds); };
     if (opts.minutes) { seconds += 60 * parseInt(opts.minutes); }
-    if (opts.hours) { seconds += 3600 * parseInt(opts.hours); };
+    if (opts.hours) { seconds += 3600 * parseInt(opts.hours); }
+    if (opts.$el) { $el = opts.$el; }
 
     return this;
   }
 
-  function start($el, opts) {
+  function start(opts) {
     if (opts) { init(opts); }
     if (interval_id) { clearInterval(interval_id); }
-
-    $el.html(toString());
 
     interval_id = setInterval(function () {
       tickDown();
       $el.html(toString());
-
-      if (seconds === 0) { $el.trigger('nextActivity'); }
+      if (seconds === 0) { $el.trigger('finished'); }
     }, 1000);
-  }
 
-  function pause() {
-
+    $el.html(toString());
   }
 
   function tickDown() {
