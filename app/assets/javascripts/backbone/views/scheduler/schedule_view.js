87911c0abbbaaa58e57530a59da1de75f5278141
JT Bowler <jbowler2400@gmail.com>
2014-08-13 23:25:07 -0700

    Refactor on render in schedule_view.js

--- app/assets/javascripts/backbone/views/scheduler/schedule_view.js
+++ app/assets/javascripts/backbone/views/scheduler/schedule_view.js
@@ -44,30 +44,30 @@
     },
 
     render: function () {
-      var self = this;
+      var json = this.model.toJSON();
 
-      if (this.model) {
-        var json = this.model.toJSON();
+      duration = json.duration * 60;
+      schedule_items = json.schedule_items;
 
-        duration = json.duration * 60;
-        schedule_items = json.schedule_items;
-        this.$el.html(this.template(json));
-        this.$el.find('.schedule-timer').html(timer.render().$el);
-
-        total_importance = 0;
-
-        _.each(schedule_items, function (item) {
-          var model = new App.Models.ScheduleItem(item);
-          var item_view = new App.Views.ScheduleItemView({ model: model });
-          self.$el.find('.schedule-table tbody').append(item_view.render().$el);
-
-          total_importance += parseInt(item.importance);
-        });
-
-        this.nextActivity();
-      }
+      this.$el.html(this.template(json));
+      this.$el.find('.schedule-timer').html(timer.render().$el);
+      this.loadScheduleItems();
+      this.nextActivity();
 
       return this;
+    },
+
+    loadScheduleItems: function () {
+      var self = this;
+      total_importance = 0;
+
+      _.each(schedule_items, function (item) {
+        var model = new App.Models.ScheduleItem(item);
+        var item_view = new App.Views.ScheduleItemView({ model: model });
+
+        self.$el.find('.schedule-table tbody').append(item_view.render().$el);
+        total_importance += parseInt(item.importance);
+      });
     },
 
     enableDisableButtons: function () {
