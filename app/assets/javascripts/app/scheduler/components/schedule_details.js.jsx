d6b64e3963d4be5ecd1d9d35f61bb9ca309bbe7a
JT Bowler <jbowler2400@gmail.com>
2015-01-27 08:29:32 -0800

    Duration is now editable on the schedule details

--- app/assets/javascripts/app/scheduler/components/schedule_details.js.jsx
+++ app/assets/javascripts/app/scheduler/components/schedule_details.js.jsx
@@ -3,12 +3,25 @@
 
   App.ScheduleDetails = React.createClass({
 
+    getInitialState: function() {
+      return { duration: this.props.duration };
+    },
+
+    _update: function (event) {
+      this.setState({ duration: event.target.value });
+    },
+
     render: function () {
       return (
         <div className='time-setter text-center'>
           <h1>{this.props.name}</h1>
           <label>
-            Total Time: <input type='text' value={this.props.duration}/>
+            Total Time:
+            <input
+              type='text'
+              value={this.state.duration}
+              onChange={this._update}
+            />
           </label>
           <br />
           <br />
