1cf477585337d041f8eaf3e48e6552c51116bea5
JT Bowler <jbowler2400@gmail.com>
2015-04-15 08:53:30 -0700

    Refactors on index javascript

--- app/assets/javascripts/app/scheduler/index.js.jsx
+++ app/assets/javascripts/app/scheduler/index.js.jsx
@@ -8,17 +8,17 @@
   App.Runner = App.Component.extend({
 
     init: function () {
-      _.bindAll(this, 'renderCurrent', 'save');
+      _.bindAll(this, 'renderCurrent');
 
-      this.initSchedule();
-      this.schedule.on('change', this.save.bind(this));
+      this.schedule = new App.Models.Schedule(this.$el.data('schedule'));
+      this.schedule.on({
+        change: function () {
+          this.schedule.save();
+          this.renderCurrent();
+        }.bind(this)
+      });
 
       return this;
-    },
-
-    initSchedule: function () {
-      this.schedule = new App.Models.Schedule(this.$el.data('schedule'));
-      this.schedule.on('change', this.renderCurrent);
     },
 
     renderCurrent: function () {
@@ -36,27 +36,19 @@
     renderConfig: function () {
       if (!this.currentViewIs('config')) { return false; }
       return (
-        <App.ScheduleConfig
-          schedule={this.schedule.toJSON()}
-        />
+        <App.ScheduleConfig schedule={this.schedule.toJSON()}/>
       );
     },
 
     renderPractice: function () {
       if (!this.currentViewIs('practice')) { return false; }
       return (
-        <App.PracticeScreen
-          schedule={this.schedule.toJSON()}
-        />
+        <App.PracticeScreen schedule={this.schedule.toJSON()}/>
       );
     },
 
     currentViewIs: function (view) {
       return view === this.schedule.get('current_view');
-    },
-
-    save: function () {
-      this.schedule.save();
     }
 
   });
