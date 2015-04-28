7d97ab36b27cac2a0ed2af5982f3fe207bcc075d
JT Bowler <jbowler2400@gmail.com>
2015-04-28 09:19:13 -0700

    Add some instructions on what to do

--- app/assets/javascripts/app/scheduler/components/settings/settings.js.jsx
+++ app/assets/javascripts/app/scheduler/components/settings/settings.js.jsx
@@ -11,6 +11,11 @@
             <App.ExampleTree/>
           </div>
           <div className='scheduler-app col-md-9'>
+            <div className='alert alert-info text-center'>
+              Choose exercises to practice and adjust the amount of time you
+              would like to spend on them.  Then click the practice
+              button at the bottom of the screen to begin!
+            </div>
             <App.ScheduleDetails {...this.props.schedule} />
             <App.ExercisesTable {...this.props.schedule} />
             <App.PracticeBtn/>
