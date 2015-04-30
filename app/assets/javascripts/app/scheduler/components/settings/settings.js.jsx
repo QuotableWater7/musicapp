e4c25dbfb945b196a5f336528ef93a63a9c12a7c
JT Bowler <jbowler2400@gmail.com>
2015-04-30 09:42:07 -0700

    Better positioning for elements in DOM

--- app/assets/javascripts/app/scheduler/components/settings/settings.js.jsx
+++ app/assets/javascripts/app/scheduler/components/settings/settings.js.jsx
@@ -5,7 +5,7 @@
 
     renderInfoMessage: function () {
       return (
-        <div className='alert alert-info text-center'>
+        <div className='alert alert-info text-center col-md-10 col-md-offset-1'>
           <b>Note:</b> Choose exercises to practice and adjust the amount of time you
           would like to spend on them.  Then click the practice
           button at the bottom of the screen to begin!
@@ -16,12 +16,14 @@
     render: function () {
       return (
         <div>
+          <div className='row'>
+            {this.renderInfoMessage()}
+          </div>
           <div className='col-md-3'>
-            <br/><br/><br/><br/><br/>
+            <br/><br/><br/>
             <App.ExampleTree/>
           </div>
           <div className='scheduler-app col-md-9'>
-            {this.renderInfoMessage()}
             <App.ScheduleDetails {...this.props.schedule} />
             <br/>
             <App.ExercisesTable {...this.props.schedule} />
