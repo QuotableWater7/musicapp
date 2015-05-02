825568e781a4f5b077a931aba8107a9008f28ae0
JT <jbowler2400@gmail.com>
2015-05-02 15:37:52 -0700

    Simplify routing stuff a little

--- app/assets/javascripts/scheduler/index.js.jsx
+++ app/assets/javascripts/scheduler/index.js.jsx
@@ -8,47 +8,33 @@
   App.Runner = App.Component.extend({
 
     init: function () {
-      _.bindAll(this, 'renderCurrent');
-
       this.schedule = new App.Models.Schedule(this.$el.data('schedule'));
       this.schedule.on({
         change: function () {
           this.schedule.save();
-          this.renderCurrent();
+          this.render_view();
         }.bind(this)
       });
 
       return this;
     },
 
-    renderCurrent: function () {
+    render_view: function () {
       React.render(
         <div>
-          {this.renderConfig()}
-          {this.renderPractice()}
+          {this['render_' + this.schedule.get('current_view')]()}
         </div>,
         this.$el[0]
       )
     },
 
-    renderConfig: function () {
-      if (!this.currentViewIs('config')) { return false; }
-      return (
-        <App.Settings {...this.schedule.toJSON()}/>
-      );
+    render_config: function () {
+      return <App.Settings {...this.schedule.toJSON()}/>;
     },
 
-    renderPractice: function () {
-      if (!this.currentViewIs('practice')) { return false; }
-      return (
-        <App.PracticeScreen {...this.schedule.toJSON()}/>
-      );
-    },
-
-    currentViewIs: function (view) {
-      return view === this.schedule.get('current_view');
+    render_practice: function () {
+      return <App.PracticeScreen {...this.schedule.toJSON()}/>;
     }
-
   });
 
   var scheduler = new App.Runner({ $el: $('.app-container') });
